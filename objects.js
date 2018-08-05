b2bac32d66baa7a3f37f106660629c0613411985
Michael Barbone <michael.barbone@gmail.com>
2018-08-05 01:27:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {Slowdive: "Alison", "My Bloody Valentine": "Somtimes"};
 
 function updatePlaylist(playlist, artist, song) {
-  playlist.PhilOchs = "Here's to the State of Mississippi";
+  playlist["Phil Ochs"] = "Here's to the State of Mississippi";
   return playlist;
 }
 
